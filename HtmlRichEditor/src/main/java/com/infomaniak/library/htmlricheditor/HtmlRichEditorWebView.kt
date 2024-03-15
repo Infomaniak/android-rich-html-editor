b9d1fb57718713f215bee0017c43bb5e70fcb077
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-03-15 15:45:02 +0100

    Make web view client easier to reuse

--- HtmlRichEditor/src/main/java/com/infomaniak/library/htmlricheditor/HtmlRichEditorWebView.kt
+++ HtmlRichEditor/src/main/java/com/infomaniak/library/htmlricheditor/HtmlRichEditorWebView.kt
@@ -3,7 +3,6 @@ package com.infomaniak.library.htmlricheditor
 import android.content.Context
 import android.util.AttributeSet
 import android.webkit.WebView
-import android.webkit.WebViewClient
 
 class HtmlRichEditorWebView @JvmOverloads constructor(
     context: Context,
@@ -16,36 +15,13 @@ class HtmlRichEditorWebView @JvmOverloads constructor(
     init {
         settings.javaScriptEnabled = true
         isFocusableInTouchMode = true
-        webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView?, url: String?) = setupDocument()
-        }
+        webViewClient = HtmlRichEditorWebViewClient()
 
         addJavascriptInterface(textFormat, "editor")
     }
 
     fun loadHtml(html: String = "") {
         super.loadDataWithBaseURL("", html, "text/html", "UTF-8", null) // TODO
-    }
-
-    private fun setupDocument() {
-        enableEdition()
-        addScript(context.readAsset("commandStatusListener.js"))
-    }
-
-    private fun enableEdition() {
-        evaluateJavascript("document.body.contentEditable = true", null)
-    }
-
-    private fun addScript(scriptCode: String) {
-        val addScriptJs = """
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.text = `${scriptCode}`;
-
-        document.head.appendChild(script);
-        """.trimIndent()
-
-        evaluateJavascript(addScriptJs, null)
     }
 
     // TODO : Use correct message in deprecated annotation
---
+++ HtmlRichEditor/src/main/java/com/infomaniak/library/htmlricheditor/HtmlRichEditorWebViewClient.kt
@@ -0,0 +1,31 @@
+package com.infomaniak.library.htmlricheditor
+
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.CallSuper
+
+open class HtmlRichEditorWebViewClient : WebViewClient() {
+    @CallSuper
+    override fun onPageFinished(view: WebView, url: String?) = view.setupDocument()
+
+    private fun WebView.setupDocument() {
+        enableEdition()
+        addScript(context.readAsset("commandStatusListener.js"))
+    }
+
+    private fun WebView.enableEdition() {
+        evaluateJavascript("document.body.contentEditable = true", null)
+    }
+
+    private fun WebView.addScript(scriptCode: String) {
+        val addScriptJs = """
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.text = `${scriptCode}`;
+
+        document.head.appendChild(script);
+        """.trimIndent()
+
+        evaluateJavascript(addScriptJs, null)
+    }
+}
