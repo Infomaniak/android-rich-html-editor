c4146f2679d3475ed84a0301bf82c23ab70e97b9
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-03-28 13:53:37 +0100

    Detect missing execCommand notifications
    This did happen for example in the case were you have a content of <h1>Hello Wolrd</h1>, and you select the word "hello" which will indicate that the state "bold" is enabled and the state "ilatic" is disabled. Now you enable "italic" and then you try disabling "bold". Bold will effectively be disabled but the "selectionchange" event listener won't pick up on it

--- HtmlRichEditor/src/main/assets/command_status_listener.js
+++ HtmlRichEditor/src/main/assets/command_status_listener.js
@@ -47,4 +47,14 @@ function areSelectionStatesTheSame(state1, state2) {
     return format.every(property => state1[property] === state2[property]) && textInfo.every(property => state1[property] === state2[property]);
 }
 
+function onAttributesChange(callback) {
+    const mutationObserver = new MutationObserver(callback);
+    const config = { subtree: true, attributes: true };
+    mutationObserver.observe(document.body, config);
+}
+
 document.addEventListener("selectionchange", reportSelectionStateChangedIfNecessary)
+
+// On some occasions execCommand modifies the attributes of tags which doesn't trigger the "selectionchange" so this has been
+// needed in order to catch some of these modifications we were lacking
+onAttributesChange(reportSelectionStateChangedIfNecessary)
