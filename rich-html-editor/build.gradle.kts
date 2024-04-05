b75429413b3f2256afc01bcbcf759a57d3e6f021
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-04-05 12:55:31 +0200

    Remove unused tests

--- rich-html-editor/build.gradle.kts
+++ rich-html-editor/build.gradle.kts
@@ -33,7 +33,4 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
