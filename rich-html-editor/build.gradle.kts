243153cd4211a77c37f7b8eda267db1f8592cbec
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-04-05 09:27:06 +0200

    Remove unused dependency in the library

--- rich-html-editor/build.gradle.kts
+++ rich-html-editor/build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
