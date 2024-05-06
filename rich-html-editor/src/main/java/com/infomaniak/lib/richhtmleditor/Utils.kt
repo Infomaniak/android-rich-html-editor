660c4860d81a3c28240caba2798e5f88185d1425
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-05-06 09:55:33 +0200

    Make the helper function readAsset internal to not expose it outside the library

--- rich-html-editor/src/main/java/com/infomaniak/lib/richhtmleditor/Utils.kt
+++ rich-html-editor/src/main/java/com/infomaniak/lib/richhtmleditor/Utils.kt
@@ -3,7 +3,7 @@ package com.infomaniak.lib.richhtmleditor
 import android.content.Context
 import java.io.BufferedReader
 
-fun Context.readAsset(fileName: String): String {
+internal fun Context.readAsset(fileName: String): String {
     return assets
         .open(fileName)
         .bufferedReader()
--- sample/src/main/java/com/infomaniak/lib/richhtmleditor/sample/FirstFragment.kt
+++ sample/src/main/java/com/infomaniak/lib/richhtmleditor/sample/FirstFragment.kt
@@ -7,9 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.infomaniak.lib.richhtmleditor.readAsset
 import com.infomaniak.lib.richhtmleditor.sample.databinding.FragmentFirstBinding
 import kotlinx.coroutines.launch
+import java.io.BufferedReader
 
 class FirstFragment : Fragment() {
 
@@ -26,8 +26,12 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?): Unit = with(binding) {
         super.onViewCreated(view, savedInstanceState)
-        // TODO: Do not access readAsset from lib
-        val customCss = requireContext().readAsset("editor_custom_css.css")
+
+        val customCss = requireContext().assets
+            .open("editor_custom_css.css")
+            .bufferedReader()
+            .use(BufferedReader::readText)
+
         editor.setHtml(testHtmlHard, customCss = listOf(customCss))
 
         buttonBold.setOnClickListener { editor.textFormat.setBold() }
