4e9bb1e45c3565c0007c0f08242851f9cf4bd92e
Abdourahamane Boinaidi <abdourahamane.boinaidi@infomaniak.com>
2024-06-06 16:41:29 +0200

    Refactor lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("sharedMinSdk", 24)
+        set("sharedCompileSdk", 34)
+        set("javaVersion", JavaVersion.VERSION_17)
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
