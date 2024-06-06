b9d82c6f38b0c040193616f29cc512516ea688ce
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-06-06 09:11:58 +0200

    Remove tests from gradle as well

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -13,8 +13,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -44,7 +42,4 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
