617ad41c69b3e63929279819289c8ccb335910d6
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2024-06-18 09:48:57 +0200

    Protect JsLifeCycleAwareExecutor against concurency

--- rich-html-editor/src/main/java/com/infomaniak/lib/richhtmleditor/executor/JsLifecycleAwareExecutor.kt
+++ rich-html-editor/src/main/java/com/infomaniak/lib/richhtmleditor/executor/JsLifecycleAwareExecutor.kt
@@ -5,11 +5,11 @@ internal abstract class JsLifecycleAwareExecutor<T> {
     private var hasDomLoaded = false
     private val objectsWaitingForDom = mutableListOf<T>()
 
-    fun executeWhenDomIsLoaded(value: T) {
+    fun executeWhenDomIsLoaded(value: T) = synchronized(lock = this) {
         if (hasDomLoaded) executeImmediately(value) else objectsWaitingForDom.add(value)
     }
 
-    fun notifyDomLoaded() {
+    fun notifyDomLoaded() = synchronized(lock = this) {
         hasDomLoaded = true
         objectsWaitingForDom.forEach(::executeImmediately)
         objectsWaitingForDom.clear()
