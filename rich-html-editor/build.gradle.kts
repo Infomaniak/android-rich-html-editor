66c612c92d29173babf4361d7891d2bc41685d81
Gibran Chevalley <gibran.chevalley@infomaniak.com>
2026-04-24 13:52:44 +0200

    chore: Bump version to 1.1.0

--- rich-html-editor/build.gradle.kts
+++ rich-html-editor/build.gradle.kts
@@ -46,7 +46,7 @@ publishing {
             }
             groupId = "com.github.infomaniak"
             artifactId = "android-rich-html-editor"
-            version = "1.0.0"
+            version = "1.1.0"
         }
     }
 }
