5e6cfe5565bcbbf08471c0bdfb7126ac16d98c9b
Jamy Bailly <jamy.bailly@infomaniak.com>
2026-09-11 09:53:19 +0200

    fix: Apply copilot comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ plugins {
     id("maven-publish")
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.nmcp) apply false
     alias(libs.plugins.nmcp.aggregation)
 }
 
